aa428016f162a38599e52a9d2a4ec0711f65778f
Justin Damon <justindamon84@gmail.com>
2022-02-08 18:34:49 -0500

    rightbar complete, layout finished

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,9 +3,60 @@ import "./rightbar.css"
 export default function Rightbar() {
     return (
         <div className="rightbar">
-            rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img className="birthdayImg" src="assets/gift.png" alt="" />
+                    <span className="birthdayText"><b>Jack Mckay</b> and <b>2 other friends</b> have a birthday today.</span>
+                </div>
+                <img className="rightbarAd" src="assets/ad.png" alt="" />
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt="" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Alec Resler</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt="" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Alec Resler</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt="" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Alec Resler</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt="" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Alec Resler</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt="" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Alec Resler</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt="" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Alec Resler</span>
+                    </li>
+                </ul>
+            </div>
         </div>
-    )
+    );
 }
  
 
