258dabba9f409e9de2bf5d64855847a9fdafd187
Justin Damon <justindamon84@gmail.com>
2022-02-22 18:26:26 -0500

    useState for like function

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,8 +1,16 @@
 import "./post.css"
 import { MoreVert } from "@mui/icons-material"
 import { Users } from "../../dummyData"
+import { useState } from "react";
 
 export default function Post({post}) {
+  const [like,setLike] = useState(post.like)
+  const [isLiked,setIsLiked] = useState(false)
+
+  const likeHandler= ()=>{
+    setLike(isLiked ? like-1 : like+1)
+    setIsLiked(!isLiked)
+  }
   return (
     <div className="post">
       <div className="postWrapper">
@@ -28,9 +36,9 @@ export default function Post({post}) {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon" src="assets/like.png" alt="" />
-            <img className="likeIcon" src="assets/heart.png" alt="" />
-            <span className="postLikeCounter">Liked By {post.like} Others</span>
+            <img className="likeIcon" src="assets/like.png" onClick={likeHandler} alt="" />
+            <img className="likeIcon" src="assets/heart.png" onClick={likeHandler} alt="" />
+            <span className="postLikeCounter">Liked By {like} Others</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment} comments</span>
