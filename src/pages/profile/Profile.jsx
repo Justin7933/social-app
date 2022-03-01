b8daa9b9c49ac5aeffab311581510e682348b7c1
Justin Damon <justindamon84@gmail.com>
2022-03-01 12:47:49 -0500

    profile page continued

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -7,15 +7,24 @@ import Rightbar from "../../components/rightbar/Rightbar";
 export default function Profile() {
   return (
     <>
-      <Topbar/>
+      <Topbar />
       <div className="profile">
-        <Sidebar/>
+        <Sidebar />
         <div className="profileRight">
-            <div className="profileRightTop">ashgh</div>
-            <div className="profileRightBottom">
-                <Feed/>
-                <Rightbar/>
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img className="profileCoverImg" src="assets/post/3.jpeg" alt="" />
+              <img className="profileUserImg" src="assets/person/1.jpeg" alt="" />
             </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">Justin Damon</h4>
+              <h4 className="profileInfoDesc">Hello My Friend</h4>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar />
+          </div>
         </div>
       </div>
     </>
